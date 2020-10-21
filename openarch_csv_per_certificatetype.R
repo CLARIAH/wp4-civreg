46aa8d2b3fa76eed65739ca0a955298fa1507ac2
RubenSchalk <rubenschalk1@gmail.com>
2020-10-21 14:52:32 +0200

    updated bso list + two simple dedup steps
    moved deduplication steps i) duplicated rows ii) duplicated SOURCE_DIGITAL_ORIGINAL to this script to get more clean output before starting the stringsim deduplication script (deduplicate.R)

--- openarch_csv_per_certificatetype.R
+++ openarch_csv_per_certificatetype.R
@@ -1,10 +1,9 @@
-
 ### This script 1) automatically downloads and unzips all openarch burgerlijke stand .zip files, 
 #               2) generates csv file per type of certificate (birth, marriage, death), 
 #                  while selecting the variables used by LINKS + a source url
 
-
 library(data.table)
+setDTthreads(threads = 8)
 
 ### BIRTH
 
@@ -12,7 +11,7 @@ setwd("C:\\Users\\Ruben\\Desktop\\openarch\\birth")
 
 # download files
 
-urls <- readLines("https://raw.githubusercontent.com/CLARIAH/wp4-civreg/master/openarch_bsg_url_list_januari2020.txt")
+urls <- readLines("https://raw.githubusercontent.com/CLARIAH/wp4-civreg/master/openarch_bsg_url_list_october2020.txt")
 
 for (url in urls) {
   download.file(url, destfile = basename(url), method="curl")
@@ -34,54 +33,31 @@ for (file in file_list){
     datasets[[file]] <- fread(file, encoding="UTF-8")[, list(EVENT_TYPE, EVENT_DAY,	EVENT_MONTH, EVENT_YEAR, EVENT_PLACE, 
                                                              SOURCE_DATE_DAY, SOURCE_DATE_MONTH, SOURCE_DATE_YEAR, SOURCE_PLACE, SOURCE_TYPE,
                                                              PR_BIR_DAY, PR_BIR_MONTH, PR_BIR_YEAR, PR_NAME_GN, PR_NAME_SPRE, PR_NAME_SURN,
-                                                             PR_OCCUPATION, PR_GENDER, PR_AGE, PR_BIR_PLACE, PR_FTHR_NAME_GN, PR_FTHR_NAME_SPRE,PR_FTHR_OCCUPATION, 
-                                                             PR_FTHR_NAME_SURN, PR_FTHR_GENDER, PR_FTHR_RELATIONTYPE, PR_FTHR_BIR_DAY, 
-                                                             PR_MTHR_RELATIONTYPE, PR_FTHR_AGE, PR_MTHR_AGE, PR_MTHR_NAME_GN, 
-                                                             PR_MTHR_NAME_SPRE, PR_MTHR_NAME_SURN, PR_MTHR_OCCUPATION, PR_MTHR_GENDER, PR_MTHR_BIR_DAY,
-                                                             SOURCEREFERENCE_PLACE, SOURCE_REMARK, EVENT_REMARK, 
+                                                             PR_OCCUPATION, PR_GENDER, PR_AGE, PR_BIR_PLACE, PR_FTHR_NAME_GN, PR_FTHR_NAME_SPRE, 
+                                                             PR_FTHR_NAME_SURN, PR_FTHR_GENDER, PR_FTHR_OCCUPATION, PR_FTHR_RELATIONTYPE,
+                                                             PR_FTHR_AGE, PR_MTHR_NAME_GN, PR_MTHR_NAME_SPRE, PR_MTHR_NAME_SURN, PR_MTHR_GENDER,
+                                                             PR_MTHR_OCCUPATION, PR_MTHR_AGE, PR_MTHR_RELATIONTYPE, SOURCEREFERENCE_PLACE, 
+                                                             SOURCEREFERENCE_DOCUMENTNUMBER, SOURCEREFERENCE_REGISTRYNUMBER, SOURCE_REMARK, EVENT_REMARK, 
                                                              SOURCEREFERENCE_INSTITUTIONNAME,SOURCE_DIGITAL_ORIGINAL,
-                                                             SOURCEREFERENCE_DOCUMENTNUMBER, SOURCE_RECORD_GUID)]
+                                                             SOURCE_RECORD_GUID)]  
   }}
 
 openarch_birth <- rbindlist(datasets)
+rm(datasets)
 
-# remove identical rows
+# remove duplicated rows
 
-openarch_birth_un <- openarch_birth[!duplicated(openarch_birth), ]
+openarch_birth <- openarch_birth[!duplicated(openarch_birth), ]
 
-# generate CLARIAH_ID (aktetype_plaats event_datum (als YYYYMMDD)_aktenummer)
+# remove rows with duplicated SOURCE_DIGITAL ORIGINAL, keep first after ordered by EVENT_YEAR
 
-openarch_birth_un[, CLARIAH_ID := paste0(substr(tolower(EVENT_TYPE), 0, 1),"_",tolower(SOURCE_PLACE),"_",EVENT_YEAR,
-                                     ifelse(EVENT_MONTH < 10, paste0("0", EVENT_MONTH), as.character(EVENT_MONTH)),
-                                     ifelse(EVENT_DAY < 10, paste0("0", EVENT_DAY), as.character(EVENT_DAY)),"_",
-                                     ifelse(SOURCEREFERENCE_DOCUMENTNUMBER == "" , paste0("NA"), SOURCEREFERENCE_DOCUMENTNUMBER)),]
+openarch_birth <- openarch_birth[order(SOURCE_DIGITAL_ORIGINAL, EVENT_YEAR, decreasing = TRUE),]
+openarch_birth <- openarch_birth[!duplicated(SOURCE_DIGITAL_ORIGINAL) ,]
 
-# replace NANANA (EVENT DATES MISSING) with SOURCE DATES in CLARIAH ID
-
-openarch_birth_un[grepl("NANANA", CLARIAH_ID), CLARIAH_ID := paste0(substr(tolower(EVENT_TYPE), 0, 1),"_",tolower(SOURCE_PLACE),"_", SOURCE_DATE_YEAR,
-                                                                ifelse(SOURCE_DATE_MONTH < 10, paste0("0", SOURCE_DATE_MONTH),
-                                                                as.character(SOURCE_DATE_MONTH)),ifelse(SOURCE_DATE_DAY < 10, 
-                                                                paste0("0", SOURCE_DATE_DAY), as.character(SOURCE_DATE_DAY)),"_", 
-                                                                ifelse(SOURCEREFERENCE_DOCUMENTNUMBER == "" , paste0("NA"), 
-                                                                SOURCEREFERENCE_DOCUMENTNUMBER)),]
-
-# replace whitespaces in CLARIAH ID SOURCE PLACE with "_"
-
-openarch_birth_un[, CLARIAH_ID := gsub(" ", "", CLARIAH_ID, fixed = TRUE),]
-
-# version 2 of clariah_id
-
-
-
-openarch_birth_un[, clariah_id2 := paste0(CLARIAH_ID,"_",substr(tolower(openarch_birth_un$PR_NAME_GN), 0, 3), substr(tolower(openarch_birth_un$PR_NAME_SURN), 0,3)) ]
-
-## if no names then also NA?
 
 # write to csv
 
-fwrite(openarch_birth_un, 
-       "C:\\Users\\Ruben\\Documents\\02. Werk\\Clariah\\openarch\\openarch_birth_unique.csv.gz", 
-       sep=";", row.names = FALSE, na = "")
+fwrite(openarch_birth, "openarch_births_2.csv.gz", sep=";", row.names = FALSE, na = "")
 
 
 
@@ -133,13 +109,19 @@ for (file in file_list){
   }}
 
 openarch_marriage <- rbindlist(datasets)
+rm(datasets)
 
-# generate CLARIAH_ID (aktetype_plaats event_datum (als YYYYMMDD)_aktenummer)
 
-openarch_marriage[, CLARIAH_ID := paste0(substr(tolower(EVENT_TYPE), 0, 1),"_",tolower(SOURCE_PLACE),"_",EVENT_YEAR,
-                                   ifelse(EVENT_MONTH < 10, paste0("0", EVENT_MONTH), as.character(EVENT_MONTH)),
-                                   ifelse(EVENT_DAY < 10, paste0("0", EVENT_DAY), as.character(EVENT_DAY)),"_",
-                                   ifelse(SOURCEREFERENCE_DOCUMENTNUMBER == "" , paste0("NA"), SOURCEREFERENCE_DOCUMENTNUMBER)),]
+# remove duplicated rows
+
+openarch_marriage <- openarch_marriage[!duplicated(openarch_birth), ]
+
+# remove rows with duplicated SOURCE_DIGITAL ORIGINAL, keep first after ordered by EVENT_YEAR
+
+openarch_marriage <- openarch_marriage[order(SOURCE_DIGITAL_ORIGINAL, EVENT_YEAR, decreasing = TRUE),]
+openarch_marriage <- openarch_marriage[!duplicated(SOURCE_DIGITAL_ORIGINAL) ,]
+
+
 
 fwrite(openarch_marriage, 
        "C:\\Users\\Ruben\\Documents\\02. Werk\\Clariah\\openarch\\openarch_marriage.csv.gz", 
@@ -183,6 +165,17 @@ for (file in file_list){
   }}
 
 openarch_death <- rbindlist(datasets)
+rm(datasets)
+
+
+# remove duplicated rows
+
+openarch_death <- openarch_death[!duplicated(openarch_birth), ]
+
+# remove rows with duplicated SOURCE_DIGITAL ORIGINAL, keep first after ordered by EVENT_YEAR
+
+openarch_death <- openarch_death[order(SOURCE_DIGITAL_ORIGINAL, EVENT_YEAR, decreasing = TRUE),]
+openarch_death <- openarch_death[!duplicated(SOURCE_DIGITAL_ORIGINAL) ,]
 
 fwrite(openarch_death, 
        "C:\\Users\\Ruben\\Documents\\02. Werk\\Clariah\\openarch\\openarch_death.csv.gz", 
