a93ef35a8c9b8bea149a2b6943e6e71f59434e89
rijpma <auke.rijpma@gmail.com>
2020-12-02 10:43:50 +0100

    simplify hisco coding

--- deathsv1/occtitles.R
+++ deathsv1/occtitles.R
@@ -1,69 +1,62 @@
 library("data.table")
+library("stringi")
 
-deaths191030 = fread(cmd = "gunzip -c ~/downloads/deaths1910-30.csv.gz")
-deaths191030 = deaths191030[, .SD, .SDcols = -patterns("HIS|OCC")]
+setDTthreads(2)
 
-deaths191030occs <- deaths191030[, .N, list(pr_occupation)]
-deaths191030occsfthr <- deaths191030[, .N, list(pr_fthr_occupation)]
+dataset = commandArgs(trailingOnly = TRUE)
+dataset = "deaths1910-30.csv"
 
-deaths191030occs <- merge(deaths191030occs, deaths191030occsfthr, by.x = "pr_occupation", by.y = "pr_fthr_occupation")
-deaths191030occs[, N := N.x + N.y]
-
-deaths191030occs$N.x <- NULL
-deaths191030occs$N.y <- NULL
-
-setnames(deaths191030occs, "pr_occupation", "occtitle")
-
-deaths191030occs <- deaths191030occs[occtitle != "" & occtitle != "--" & occtitle != "-"][order(-N)]
-rm(deaths191030occsfthr)
-
-# read hsnhisco (from xlsx since cvs gives issues)
-
-hsnhisco <- fread("HSN_HISCO_release_2020_02.csv") 
-                      
+deaths191030 = fread(dataset)
+deaths191030 = deaths191030[, .SD, .SDcols = -patterns("OCC|HIS")]
+hsnhisco <- fread("HSN_HISCO_release_2020_02.csv")
 
 # standardize and merge occs deaths with hsnhisco
 
-deaths191030occs[,occtitle_st := tolower(occtitle)]
-deaths191030occs[,occtitle_st := gsub('[[:punct:] ]+',' ',occtitle_st)]
-deaths191030occs[,occtitle_st := trimws(occtitle_st, which = c("both"))]
-deaths191030occs[,occtitle_st := gsub(" ", "", occtitle_st)]
-deaths191030occs <- deaths191030occs[!duplicated(occtitle_st),]
+deaths191030[, occtitle_st := tolower(occtitle)]
+deaths191030[, occtitle_st := gsub('[[:punct:] ]+',' ',occtitle_st)]
+deaths191030[, occtitle_st := trimws(occtitle_st, which = c("both"))]
+deaths191030[, occtitle_st := gsub(" ", "", occtitle_st)]
 
-hsnhisco[,original_st := tolower(Original)]
-hsnhisco[,original_st := gsub('[[:punct:] ]+',' ',original_st)]
-hsnhisco[,original_st := trimws(original_st, which = c("both"))]
-hsnhisco[,original_st := gsub(" ", "", original_st)]
+hsnhisco[, original_st := tolower(Original)]
+hsnhisco[, original_st := gsub('[[:punct:] ]+',' ',original_st)]
+hsnhisco[, original_st := trimws(original_st, which = c("both"))]
+hsnhisco[, original_st := gsub(" ", "", original_st)]
 hsnhisco <- hsnhisco[order(original_st, -HISCO),]
 hsnhisco <- hsnhisco[!duplicated(original_st),]
 
-
-hsnmatch <- merge(deaths191030occs, hsnhisco[,c("HISCO", "HISCLASS", "HISCLASS_5", "HISCAM_NL", "original_st", "OCC1950")], by.x = "occtitle_st", by.y = "original_st", all.x = T, all.y = F)
+deaths191030 <- merge(
+    deaths191030, 
+    hsnhisco[,c("HISCO", "HISCLASS", "HISCLASS_5", "HISCAM_NL", "original_st", "OCC1950")], 
+    by.x = "occtitle_st", 
+    by.y = "original_st", 
+    all.x = TRUE, 
+    all.y = FALSE)
 
 # check for duplids & code and seperate multiple matches
 
-hsnmatch[duplicated(occtitle_st),.N]
-
 # fix hisclass fishermen (8 instead of 10)
 
-hsnmatch[HISCO == 64100 & !grepl("knecht", occtitle), HISCLASS := 8 ]
-hsnmatch[HISCO == 64100 & !grepl("knecht", occtitle), HISCLASS_5 := 3 ]
+deaths191030[HISCO == 64100 & !grepl("knecht", occtitle), HISCLASS := 8 ]
+deaths191030[HISCO == 64100 & !grepl("knecht", occtitle), HISCLASS_5 := 3 ]
 
 # save occtitle coding
-
 # fwrite(hsnmatch, "deaths1910-30_occtitles_coded.csv", sep = ";", row.names = FALSE)
 
 # merge occs with deaths191030 for ego's only
 
-deaths191030[,occtitle_st := tolower(pr_occupation)]
-deaths191030[,occtitle_st := gsub('[[:punct:] ]+',' ',occtitle_st)]
-deaths191030[,occtitle_st := trimws(occtitle_st, which = c("both"))]
-deaths191030[,occtitle_st := gsub(" ", "", occtitle_st)]
-
-deaths191030 <- merge(deaths191030, hsnmatch, by = "occtitle_st", all.x = TRUE, all.y = FALSE)
-
 deaths191030[!is.na(HISCO), .N] # 99% of cert with occtitles coded into hisco
 deaths191030[, sum(!is.na(HISCO)) / .N] # 99% of cert with occtitles coded into hisco
+
+tail(deaths191030[, .N, by = list(HISCO)][order(N)], 10)
+deaths191030[HISCO == 61220, .(occtitle, occtitle_st)]
+hsnhisco[HISCO == 61220, ]
+deaths191030[HISCO == 99900, .(occtitle, occtitle_st)]
+hsnhisco[HISCO == 99900, ]
+deaths191030[HISCO == 41025, .(occtitle, occtitle_st)]
+hsnhisco[HISCO == 41025, ]
+deaths191030[HISCO == -3, .(occtitle, occtitle_st)]
+deaths191030[occtitle_st == ""]
+hsnhisco[HISCO == -3, ]
 
 # add n > 100 without hisco manually (female occs)
 
@@ -99,3 +92,7 @@ deaths191030[, N.x := NULL]
 deaths191030[, N.y := NULL]
 
 names(deaths191030) <- sub(".x", "", names(deaths191030))
+
+fwrite(
+    x = deaths191030, 
+    file = dataset)
