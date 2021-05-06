6e410ad9c7a556169d6094f4b9a682d8ac2394b2
RubenSchalk <rubenschalk1@gmail.com>
2021-05-06 21:14:13 +0200

    Create crop_left_pages.py
    # crops image to relevant text areo only for Leiden income tax, left-hand pages
    # TODO: make 1 python loop for left and right

---
+++ OCRtest/crop_left_pages.py
@@ -0,0 +1,35 @@
+# crops image to relevant text areo only for Leiden income tax, left-hand pages
+# TODO: make 1 python loop for left and right
+
+import cv2
+import glob
+import os
+import numpy as np
+
+# Get list of filenames to convert
+files = glob.glob("*_left")
+print(files)
+
+import cv2
+import glob
+import os.path
+
+path = "*_left.jpg"
+
+for file in glob.glob(path):
+
+# left:
+    image = cv2.imread(file)
+
+
+    y=300 # left
+    x=180 # top
+    h=1700 # right
+    w=2100 # bottom
+
+    crop_image = image[x:w, y:h]
+
+    basename = os.path.basename(file)  # e.g. MyPhoto.jpg
+    name = os.path.splitext(basename)[0]  # e.g. MyPhoto
+    print(name)
+    cv2.imwrite(name + "_cropped.png", crop_image)
