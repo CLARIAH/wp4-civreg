82e788c559bf1aad889f924054f2fd63b17947e5
RubenSchalk <rubenschalk1@gmail.com>
2021-06-03 10:25:57 +0200

    jp2 to jpeg conversion

---
+++ OCRtest/image_convert.py
@@ -0,0 +1,10 @@
+import glob
+from PIL import Image
+import os
+
+images = glob.glob("C:\\Users\\Ruben\\Documents\\02. Werk\\Clariah\\Pilots\\RAL 1698 inv. 13\\*.jp2")
+for image in images:
+    with open(image, "rb") as file:
+        img = Image.open(file)
+        name = os.path.splitext(image)[0] + '.jpg'
+        img.save(name, 'JPEG')
